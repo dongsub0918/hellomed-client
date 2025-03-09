b595ef81880b8be7221827a8452d34adcc361cfe
김동섭 <dongsubk@umich.edu>
2025-03-08 22:18:43 -0500

    sitemap has been added

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+// next-sitemap.mjs
+
+module.exports = {
+  siteUrl: "https://www.hello-med.com", // Your website URL
+  generateRobotsTxt: true, // Optional: generate robots.txt automatically
+  // Additional options can be added as needed
+};
--- next.config.mjs
+++ next.config.mjs
@@ -13,6 +13,18 @@ const nextConfig = {
     ],
   },
   reactStrictMode: false,
+
+  async redirects() {
+    const redirects = {
+      "/strep-sinusitis": "/acute-conditions/strep",
+    };
+
+    return Object.keys(redirects).map((oldUrl) => ({
+      source: oldUrl,
+      destination: redirects[oldUrl],
+      permanent: true, // This sends a 301 redirect
+    }));
+  },
 };
 
 export default nextConfig;
