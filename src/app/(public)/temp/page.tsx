b6e633b4023335c2b7f1dbc84452a61767f7c245
김동섭 <dongsubk@umich.edu>
2025-12-13 12:37:44 +0900

    feat: start designing new landing page

---
+++ src/app/(public)/temp/page.tsx
@@ -0,0 +1,14 @@
+import styles from "./page.module.css";
+import Footer from "@/ui/footer";
+
+export default function TempLandingPage() {
+  return (
+    <main>
+      <nav>Navbar for landing</nav>
+      <section>Hero (chatbot) section</section>
+      <section>Links, carousel section</section>
+      <section>Locations section</section>
+      <Footer />
+    </main>
+  );
+}
