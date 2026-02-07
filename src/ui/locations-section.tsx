f9d6b7558d93b3d5f585f04d16c074e349239631
김동섭 <dongsubk@umich.edu>
2026-02-06 20:42:01 -0500

    Fix: widen navbar links gap + Image href error

--- src/ui/locations-section.tsx
+++ src/ui/locations-section.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 const locations = [
   {
     name: "HELLOMED Central",
-    imageUrl: "central-location.png",
+    imageUrl: "/central-location.png",
     mapUrl:
       "https://www.google.com/maps/place/HELLOMED+CENTRAL/@42.2794416,-83.742861,17.41z/data=!3m1!5s0x883cae40822b66c1:0xa12c8057c45fcd20!4m6!3m5!1s0x883cafdbcba9f80d:0xe73623ae4cab907e!8m2!3d42.2794509!4d-83.7411722!16s%2Fg%2F11qf_v5z15?entry=ttu",
     appointmentUrl:
@@ -14,7 +14,7 @@ const locations = [
   },
   {
     name: "HELLOMED North",
-    imageUrl: "north-location.png",
+    imageUrl: "/north-location.png",
     mapUrl:
       "https://www.google.com/maps/place/HELLOMED+North/@42.3037993,-83.7089182,16.7z/data=!3m1!5s0x883cac265eb73f1f:0xe91c7cb229ccc520!4m6!3m5!1s0x883cac2654df9761:0x82dccbb38ec4cca!8m2!3d42.3036726!4d-83.7063491!16s%2Fg%2F1pt_kvcq7?entry=ttu",
     appointmentUrl:
@@ -23,7 +23,7 @@ const locations = [
   },
   {
     name: "HELLOMED South (inside Meijer)",
-    imageUrl: "meijer's-location.png",
+    imageUrl: "/meijer's-location.png",
     mapUrl:
       "https://www.google.com/maps/place/HELLOMED+South(Inside+Meijer)+Urgent+Care+Ann+Arbor/@42.2398356,-83.7690996,17z/data=!3m1!4b1!4m6!3m5!1s0x883cb16d1922f951:0x3491d30ab536b7e9!8m2!3d42.2398356!4d-83.7665247!16s%2Fg%2F11x38v5w5c?entry=ttu&g_ep=EgoyMDI1MDUwNS4wIKXMDSoASAFQAw%3D%3D",
     appointmentUrl:
@@ -32,7 +32,7 @@ const locations = [
   },
   {
     name: "Telemedicine",
-    imageUrl: "telemedicine-location.jpg",
+    imageUrl: "/telemedicine-location.jpg",
     mapUrl: "",
     internalUrl: "/urgent-care/telemedicine",
     appointmentUrl:
@@ -41,7 +41,7 @@ const locations = [
   },
   {
     name: "Immigration Medical Exam",
-    imageUrl: "immigration-medical-exam-uscis.jpg",
+    imageUrl: "/immigration-medical-exam-uscis.jpg",
     mapUrl: "",
     internalUrl: "/immigration-medical-exam",
     appointmentUrl:
